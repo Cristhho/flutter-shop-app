0154ef29f31522207d8683200d03680ea8c6ce81
cristhian ochoa <cristhho@gmail.com>
2021-09-01 16:55:28 -0500

    products overview

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String image;
+
+  ProductItem(this.id, this.title, this.image);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        image,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
