92f5ef152a565b41c6bc1db2e7f39deeccc80921
cristhian ochoa <cristhho@gmail.com>
2021-09-02 16:06:33 -0500

    products provider, change notifier provider in main.dart

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+import './product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (context, index) => ProductItem(
+        products[index].id,
+        products[index].title,
+        products[index].imageUrl
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0
+      ),
+    );
+  }
+}
