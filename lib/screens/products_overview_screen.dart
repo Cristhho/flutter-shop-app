edfaeb0fe6e56b39d826833f5d56adc02f09accf
cristhian ochoa <cristhho@gmail.com>
2021-09-17 17:49:36 -0500

    favorites filter

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,14 +2,43 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var showOnlyFavorites = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(child: Text('Only Favorites'), value: FilterOptions.Favorites,),
+              PopupMenuItem(child: Text('Show all'), value: FilterOptions.All,),
+            ],
+            onSelected: (FilterOptions selected) {
+              setState(() {
+                if (selected == FilterOptions.Favorites) {
+                  showOnlyFavorites = true;
+                } else {
+                  showOnlyFavorites = false;
+                }
+              });
+            },
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(showOnlyFavorites),
     );
   }
 }
