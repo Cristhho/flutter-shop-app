29dcb8b5d24faa768919a0d79ee91f4cd1fbe902
cristhian ochoa <cristhho@gmail.com>
2021-09-17 19:24:12 -0500

    cart provider

---
+++ lib/providers/cart.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(productId, (value) => CartItem(id: value.id, title: value.title, quantity: value.quantity + 1, price: value.price));
+    } else {
+      _items.putIfAbsent(productId, () => CartItem(id: DateTime.now().toString(), title: title, quantity: 1, price: price));
+    }
+  }
+}
