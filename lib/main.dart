808bcb145d0aa546c9bbd95c9ab5fedd7ffb32ce
christian <cristhho@gmail.com>
2023-02-15 16:12:04 -0500

    auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'screens/cart_screen.dart';
 import 'screens/orders_screen.dart';
 import 'screens/user_products_screen.dart';
 import 'screens/edit_product_screen.dart';
+import 'screens/auth_screen.dart';
 import 'providers/products.dart';
 import 'providers/cart.dart';
 import 'providers/orders.dart';
@@ -35,13 +36,14 @@ class MyApp extends StatelessWidget {
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato'
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName : (ctx) => CartScreen(),
           OrdersScreen.routeName : (ctx) => OrdersScreen(),
           UserProductsScreen.routeName : (ctx) => UserProductsScreen(),
           EditProductScreen.routeName : (ctx) => EditProductScreen(),
+          //AuthScreen.routeName : (ctx) => AuthScreen(),
         },
       ),
     );
